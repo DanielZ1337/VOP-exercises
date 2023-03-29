2d7235b993a972c3507b3f64b9d35a8927c275b2
DanielZ1337 <danielz2nd@hotmail.com>
2023-03-29 02:01:37 +0200

    prep exercise 9 done

---
+++ Preparation Exercises/Recursive-Lecture-9/src/main/java/_18_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class _18_2 {
+    public static void main(String[] args) {
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an index for a Fibonacci number: ");
+        int index = input.nextInt();
+
+        // Find and display the Fibonacci number
+        System.out.println("The Fibonacci number at index "
+                + index + " is " + fib(index));
+    }
+
+    public static long fib(long n){
+        long currentFib = 0;
+        long f0 = 0; // For fib(0)
+        long f1 = 1; // For fib(1)
+        for (int i = 1; i < n; i++) {
+            currentFib = f0 + f1;
+            f0 = f1;
+            f1 = currentFib;
+        }
+        return currentFib;
+    }
+}
